81a4d9258660267ab12b8aaaa5101a9ffbfdcd15
af1tgc <af1tgc@gmail.com>
2020-09-16 13:35:55 +0900

    200916_bypassJB

---
+++ 2_iOS/bypassJB.js
@@ -0,0 +1,22 @@
+if(ObjC.available){
+	send("\nbypassJB() is activated....\n\n")
+	var classHook = ObjC.classes.IntroViewController["- isJailbroken"];
+
+	Interceptor.attach(classHook.implementation, {
+		onEnter: function(args) {
+			console.log("\n")
+			console.log("\t[\tfunc. OnEnter\t]\n\n")
+		},
+
+		onLeave: function(retval){
+			console.log("\t[\tfunc. OnLeave\t]\n\n")
+			console.log("\t[*] "+retval+" ; (return value of func.)")
+			retval.replace(0x0)
+			console.log("\t[+] "+retval+" ; (Injected RET.)")
+		}
+
+	});
+
+}else{
+	console.log("[Warning] Injection Failed\nbypassJB() is deactivated.\n\n\n")
+}
